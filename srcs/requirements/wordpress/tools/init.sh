e5823f2cd3e9c4afc41c592c865b5bde11618140
kinamura <i.kosuke0801@gmail.com>
2026-01-12 23:14:56 +0900

    add: user profile

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# wp-config.phpが存在しなければ作成
-if [ ! -f /var/www/html/wordpress/wp-config.php ]; then
-    cat > /var/www/html/wordpress/wp-config.php << EOF
+cd /var/www/html/wordpress
+
+# wp-config.php
+if [ ! -f wp-config.php ]; then
+	cat > wp-config.php << EOF
 <?php
 define('DB_NAME', '${MYSQL_DATABASE}');
 define('DB_USER', '${MYSQL_USER}');
@@ -10,18 +12,36 @@ define('DB_PASSWORD', '${MYSQL_PASSWORD}');
 define('DB_HOST', '${MYSQL_HOST}');
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
-
 \$table_prefix = 'wp_';
-
 define('WP_DEBUG', false);
-
 if ( ! defined( 'ABSPATH' ) ) {
-    define( 'ABSPATH', __DIR__ . '/' );
+	define( 'ABSPATH', __DIR__ . '/' );
 }
-
 require_once ABSPATH . 'wp-settings.php';
 EOF
-    chown www-data:www-data /var/www/html/wordpress/wp-config.php
+	chown www-data:www-data wp-config.php
+
+	# sleep to wake up mariadb
+	while ! mysqladmin ping -h mariadb --silent; do
+		sleep 1
+	done
+
+	# install WordPress
+	wp core install \
+		--url="${DOMAIN_NAME}" \
+		--title="Inception" \
+		--admin_user="${WP_ADMIN_USER}" \
+		--admin_password="${WP_ADMIN_PASSWORD}" \
+		--admin_email="${WP_ADMIN_EMAIL}" \
+		--allow-root
+
+	# create user
+	wp user create \
+		"${WP_USER}" \
+		"${WP_USER_EMAIL}" \
+		--user_pass="${WP_USER_PASSWORD}" \
+		--role=author \
+		--allow-root
 fi
 
 exec php-fpm8.2 -F
